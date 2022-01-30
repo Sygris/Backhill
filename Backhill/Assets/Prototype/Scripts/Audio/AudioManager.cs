e6cf839e3e19e1f61395bab0f361122dbf380cc6
LukyLewis2 <lewis.cozens@hotmail.co.uk>
2022-01-30 21:44:44 +0000

    # Audio made so much better than when Tiago did it

--- Backhill/Assets/Prototype/Scripts/Audio/AudioManager.cs
+++ Backhill/Assets/Prototype/Scripts/Audio/AudioManager.cs
@@ -6,10 +6,10 @@ public class AudioManager : MonoBehaviour
 {
     public static AudioManager instance;
 
-    [Header("Dictionary Settings")]
-    public List<string> clipName = new List<string>();
-    public List<AudioClip> clipList = new List<AudioClip>();
-    private Dictionary<string, AudioClip> clipLib = new Dictionary<string, AudioClip>();
+    //[Header("Dictionary Settings")]
+    //public List<string> clipName = new List<string>();
+    //public List<AudioClip> clipList = new List<AudioClip>();
+    //private Dictionary<string, AudioClip> clipLib = new Dictionary<string, AudioClip>();
 
     [Header("Pool Settings")]
     public GameObject prefabToPool;
@@ -20,53 +20,53 @@ public class AudioManager : MonoBehaviour
     {
         instance = this;
 
-        for (int i = 0; i < clipName.Count; i++)
-            clipLib.Add(clipName[i], clipList[i]);
+        //for (int i = 0; i < clipName.Count; i++)
+        //    clipLib.Add(clipName[i], clipList[i]);
 
         CreateInstances();
     }
 
-    public void PlaySound(string clip, Vector3 position, float volume = 1)
-    {
-        GameObject prefab = GetPoolObject();
+    //public void PlaySound(string clip, Vector3 position, float volume = 1)
+    //{
+    //    GameObject prefab = GetPoolObject();
 
-        if (prefab == null) return;
+    //    if (prefab == null) return;
 
-        prefab.transform.position = position;
-        prefab.SetActive(true);
+    //    prefab.transform.position = position;
+    //    prefab.SetActive(true);
 
-        AudioSource prefabAudioSource = prefab.GetComponent<AudioSource>();
+    //    AudioSource prefabAudioSource = prefab.GetComponent<AudioSource>();
 
-        prefabAudioSource.clip = clipLib[clip];
-        prefabAudioSource.volume = volume;
-        prefabAudioSource.Play();
+    //    prefabAudioSource.clip = clipLib[clip];
+    //    prefabAudioSource.volume = volume;
+    //    prefabAudioSource.Play();
 
-        StartCoroutine(BackToPool(prefab, clipLib[clip].length));
-    }
+    //    StartCoroutine(BackToPool(prefab, clipLib[clip].length));
+    //}
 
-    public void PlaySound(AudioClip clip, Vector3 position, out float duration, float volume = 1)
-    {
-        GameObject prefab = GetPoolObject();
+    //public void PlaySound(AudioClip clip, Vector3 position, out float duration, float volume = 1)
+    //{
+    //    GameObject prefab = GetPoolObject();
 
-        if (prefab == null)
-        {
-            duration = 0;
-            return;
-        }
+    //    if (prefab == null)
+    //    {
+    //        duration = 0;
+    //        return;
+    //    }
 
-        prefab.transform.position = position;
-        prefab.SetActive(true);
+    //    prefab.transform.position = position;
+    //    prefab.SetActive(true);
 
-        AudioSource prefabAudioSource = prefab.GetComponent<AudioSource>();
+    //    AudioSource prefabAudioSource = prefab.GetComponent<AudioSource>();
 
-        prefabAudioSource.clip = clip;
-        prefabAudioSource.volume = volume;
-        prefabAudioSource.Play();
+    //    prefabAudioSource.clip = clip;
+    //    prefabAudioSource.volume = volume;
+    //    prefabAudioSource.Play();
 
-        StartCoroutine(BackToPool(prefab, clip.length));
+    //    StartCoroutine(BackToPool(prefab, clip.length));
 
-        duration = clip.length;
-    }
+    //    duration = clip.length;
+    //}
 
     public void PlaySound(AudioClip clip, Vector3 position, float volume = 1)
     {
@@ -86,28 +86,23 @@ public class AudioManager : MonoBehaviour
         StartCoroutine(BackToPool(prefab, clip.length));
     }
 
-    public void PlaySound(string clip, GameObject parent, float volume = 1)
-    {
-        GameObject prefab = GetPoolObject();
-
-        if (prefab == null) return;
-
-        #region Configure Prefab
-        prefab.transform.position = parent.transform.position;
-        prefab.transform.parent = parent.transform;
-        prefab.SetActive(true);
-        #endregion
-
-        #region Configure AudioSource
-        AudioSource prefabAudioSource = prefab.GetComponent<AudioSource>();
-
-        prefabAudioSource.clip = clipLib[clip];
-        prefabAudioSource.volume = volume;
-        prefabAudioSource.Play();
-        #endregion
-
-        StartCoroutine(BackToPool(prefab, clipLib[clip].length, true));
-    }
+    //public void PlaySound(string clip, GameObject parent, float volume = 1)
+    //{
+    //    GameObject prefab = GetPoolObject();
+    //    if (prefab == null) return;
+    //    #region Configure Prefab
+    //    prefab.transform.position = parent.transform.position;
+    //    prefab.transform.parent = parent.transform;
+    //    prefab.SetActive(true);
+    //    #endregion
+    //    #region Configure AudioSource
+    //    AudioSource prefabAudioSource = prefab.GetComponent<AudioSource>();
+    //    prefabAudioSource.clip = clipLib[clip];
+    //    prefabAudioSource.volume = volume;
+    //    prefabAudioSource.Play();
+    //    #endregion
+    //    StartCoroutine(BackToPool(prefab, clipLib[clip].length, true));
+    //}
 
     private void CreateInstances()
     {
--- Backhill/Assets/Prototype/Scripts/GameEvents/Door/DoorOpenTrigger.cs
+++ Backhill/Assets/Prototype/Scripts/GameEvents/Door/DoorOpenTrigger.cs
@@ -5,7 +5,7 @@ using UnityEngine;
 public class DoorOpenTrigger : MonoBehaviour
 {
     [SerializeField] private int _triggerID;
-    [SerializeField] private string _sfxTitle;
+    [SerializeField] private AudioClip _sfxTitle;
     private void OnTriggerEnter(Collider other)
     {
         AudioManager.instance.PlaySound(_sfxTitle, transform.position, 0.5f);
