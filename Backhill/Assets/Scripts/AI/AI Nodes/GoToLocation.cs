ecd0d3bf1198606767548829a9daf93d671550cc
LukyLewis2 <lewis.cozens@hotmail.co.uk>
2022-03-07 17:48:33 +0000

    + Tutorial HUD Added

--- Backhill/Assets/Scripts/AI/AI Nodes/GoToLocation.cs
+++ Backhill/Assets/Scripts/AI/AI Nodes/GoToLocation.cs
@@ -15,18 +15,18 @@ public class GoToLocation : Node
 
     public override NodeState Decision()
     {
-        if (_target != null) // If theres no target then this node failes
-        {
-            float distance = Vector3.Distance(_target.transform.position, _agent.transform.position);
-            if (distance > /*BlackBoard._minDistance*/) // If you're further than min distance move to the target
-            {
-                //_agentActions.MoveTo(_target.transform.position);
-                return NodeState.RUNNING;
-            }
-            else
-                return NodeState.SUCCESS;
-        }
-        else
-            return NodeState.FAILURE;
+        //if (_target != null) // If theres no target then this node failes
+        //{
+        //    float distance = Vector3.Distance(_target.transform.position, _agent.transform.position);
+        //    if (distance > /*BlackBoard._minDistance*/) // If you're further than min distance move to the target
+        //    {
+        //        //_agentActions.MoveTo(_target.transform.position);
+        //        return NodeState.RUNNING;
+        //    }
+        //    else
+        return NodeState.SUCCESS;
+        //}
+        //else
+        //    return NodeState.FAILURE;
     }
 }
