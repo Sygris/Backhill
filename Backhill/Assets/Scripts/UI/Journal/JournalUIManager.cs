eacebe722f56ca59a1681a385bb39c3de53efa3e
Tiago Antunes <tiago.a.vista@gmail.com>
2022-03-21 04:07:03 +0000

    Note UI is working

---
+++ Backhill/Assets/Scripts/UI/Journal/JournalUIManager.cs
@@ -0,0 +1,45 @@
+using TMPro;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class JournalUIManager : MonoBehaviour, IPointerEnterHandler, IPointerExitHandler, IPointerClickHandler, IPointerDownHandler, IPointerUpHandler
+{
+    [Header("UI References")]
+    [SerializeField] private TextMeshProUGUI _description;
+
+    [Header("Note Color Settings")]
+    [SerializeField] private Color _tabIdle;
+    [SerializeField] private Color _tabHover;
+    [SerializeField] private Color _tabSelected;
+
+    public void OnPointerClick(PointerEventData eventData)
+    {
+        int index = eventData.pointerCurrentRaycast.gameObject.transform.GetSiblingIndex();
+        if (InventorySystem.Instance.Inventory[ItemType.Note].Count <= 0 || index > (InventorySystem.Instance.Inventory[ItemType.Note].Count - 1)) return;
+
+        _description.text = InventorySystem.Instance.Inventory[ItemType.Note][index].Data.Description;
+    }
+
+    public void OnPointerDown(PointerEventData eventData)
+    {
+        return;
+    }
+
+    public void OnPointerEnter(PointerEventData eventData)
+    {
+        int index = eventData.pointerCurrentRaycast.gameObject.transform.GetSiblingIndex();
+
+        if (InventorySystem.Instance.Inventory[ItemType.Note].Count <= 0 || index > (InventorySystem.Instance.Inventory[ItemType.Note].Count - 1)) return;
+    }
+
+    public void OnPointerExit(PointerEventData eventData)
+    {
+        _description.text = string.Empty;
+        return;
+    }
+
+    public void OnPointerUp(PointerEventData eventData)
+    {
+        return;
+    }
+}
