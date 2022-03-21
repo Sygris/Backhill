b5540c996529783b275a1f3dd8efc2f096f4b5f9
LukyLewis2 <lewis.cozens@hotmail.co.uk>
2022-03-21 14:03:26 +0000

    # Test Push

--- Backhill/Assets/Scripts/Inventory System/UIInventoryItemSlot.cs
+++ Backhill/Assets/Scripts/Inventory System/UIInventoryItemSlot.cs
@@ -5,14 +5,14 @@ using UnityEngine.UI;
 public class UIInventoryItemSlot : MonoBehaviour
 {
     [SerializeField] private Image _icon;
-    [SerializeField] private TextMeshProUGUI _label;
+    //[SerializeField] private TextMeshProUGUI _label;
     [SerializeField] private GameObject _stackObject;
     [SerializeField] private TextMeshProUGUI _stackLabel;
 
     public void Set(InventoryItem item)
     {
         _icon.sprite = item.Data.Icon;
-        _label.text = item.Data.DisplayName;
+        //_label.text = item.Data.DisplayName;
 
         _stackLabel.text = item.StackSize.ToString();
         
