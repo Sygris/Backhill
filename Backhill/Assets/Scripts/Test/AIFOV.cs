ce21d73a6de3b585e08f24b78fec3e6eb9cdcf77
LukyLewis2 <lewis.cozens@hotmail.co.uk>
2022-03-22 15:33:48 +0000

    # Test Push

--- Backhill/Assets/Scripts/Test/AIFOV.cs
+++ Backhill/Assets/Scripts/Test/AIFOV.cs
@@ -1,43 +1,43 @@
-using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+//using System.Collections;
+//using System.Collections.Generic;
+//using UnityEngine;
 
-public class AIFOV : MonoBehaviour
-{
-    public LayerMask targetMask;
-    public LayerMask obstacleMask;
+//public class AIFOV : MonoBehaviour
+//{
+//    public LayerMask targetMask;
+//    public LayerMask obstacleMask;
 
-    [Range(0, 360)]
-    public float viewAngle;
+//    [Range(0, 360)]
+//    public float viewAngle;
 
-    public float detectionRadius;
+//    public float detectionRadius;
 
-    private void Update()
-    {
-        Collider[] targetsInViewRadius = Physics.OverlapSphere(transform.position, detectionRadius, targetMask);
+//    private void Update()
+//    {
+//        Collider[] targetsInViewRadius = Physics.OverlapSphere(transform.position, detectionRadius, targetMask);
 
-        for (int i = 0; i < targetsInViewRadius.Length; i++)
-        {
-            Transform target = targetsInViewRadius[i].transform;
-            Vector3 dirToTarget = (target.position - transform.position).normalized;
+//        for (int i = 0; i < targetsInViewRadius.Length; i++)
+//        {
+//            Transform target = targetsInViewRadius[i].transform;
+//            Vector3 dirToTarget = (target.position - transform.position).normalized;
 
-            if (Vector3.Angle(transform.forward, dirToTarget) < viewAngle / 2)
-            {
-                float distance = Vector3.Distance(transform.position, target.position);
+//            if (Vector3.Angle(transform.forward, dirToTarget) < viewAngle / 2)
+//            {
+//                float distance = Vector3.Distance(transform.position, target.position);
 
-                if (!Physics.Raycast(transform.position, dirToTarget, distance, obstacleMask))
-                {
-                    //Debug.Log("Spotted");
-                }
-            }
-        }
-    }
+//                if (!Physics.Raycast(transform.position, dirToTarget, distance, obstacleMask))
+//                {
+//                    //Debug.Log("Spotted");
+//                }
+//            }
+//        }
+//    }
 
-    public Vector3 DirFromAngle(float angleInDegrees, bool angleIsGlobal)
-    {
-        if (!angleIsGlobal)
-            angleInDegrees += transform.eulerAngles.y;
+//    public Vector3 DirFromAngle(float angleInDegrees, bool angleIsGlobal)
+//    {
+//        if (!angleIsGlobal)
+//            angleInDegrees += transform.eulerAngles.y;
 
-        return new Vector3(Mathf.Sin(angleInDegrees * Mathf.Deg2Rad), 0, Mathf.Cos(angleInDegrees * Mathf.Deg2Rad));
-    }
-}
+//        return new Vector3(Mathf.Sin(angleInDegrees * Mathf.Deg2Rad), 0, Mathf.Cos(angleInDegrees * Mathf.Deg2Rad));
+//    }
+//}
