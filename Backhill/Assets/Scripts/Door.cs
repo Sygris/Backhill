d55fd1cdf2d12d37b507834db44aa75a440c23ac
Tiago Antunes <tiago.a.vista@gmail.com>
2022-03-22 00:43:13 +0000

    Revert "Revert "Door with key""
    This reverts commit ad18ed2b7dc27c87f44cd926b6542fd130c97a21.

---
+++ Backhill/Assets/Scripts/Door.cs
@@ -0,0 +1,25 @@
+using UnityEngine;
+
+public class Door : MonoBehaviour
+{
+    [SerializeField] InventoryItemData _item;
+    [SerializeField] Animator _animator;
+    [SerializeField] string _animationName;
+
+    private void Start()
+    {
+        _animator = GetComponent<Animator>();
+    }
+
+    public void Open()
+    {
+        for (int i = 0; i < InventorySystem.Instance.Inventory[ItemType.Item].Count; i++)
+        {
+            if (InventorySystem.Instance.Inventory[ItemType.Item][i].Data.Id == _item.Id)
+            {
+                _animator.Play(_animationName);
+                InventorySystem.Instance.Inventory[ItemType.Item][i].RemoveFromStack();
+            }
+        }
+    }
+}
