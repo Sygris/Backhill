046404f2e9602d44b7729d5b30028419e985daff
Tiago Antunes <tiago.a.vista@gmail.com>
2022-03-24 12:50:50 +0000

    Objectives are now completed

--- Backhill/Assets/Scripts/Objectives/TriggerObjective.cs
+++ Backhill/Assets/Scripts/Objectives/TriggerObjective.cs
@@ -5,11 +5,17 @@ public class TriggerObjective : Objective
 {
     [SerializeField] private string _playerTag = "Player";
 
+    private void Start()
+    {
+        GetComponent<BoxCollider>().isTrigger = true;
+    }
+
     private void OnTriggerEnter(Collider other)
     {
         if (other.gameObject.CompareTag(_playerTag))
         {
             Complete();
+            GetComponent<BoxCollider>().enabled = false;
         }
     }
 }
