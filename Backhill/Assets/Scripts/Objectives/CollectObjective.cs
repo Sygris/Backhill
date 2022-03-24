e36d87bb224d8223b909e39a52b7265b880b8bd3
Tiago Antunes <tiago.a.vista@gmail.com>
2022-03-24 12:20:02 +0000

    Half of the objectives in post-tutorial are implemented

--- Backhill/Assets/Scripts/Objectives/CollectObjective.cs
+++ Backhill/Assets/Scripts/Objectives/CollectObjective.cs
@@ -2,13 +2,15 @@ using UnityEngine;
 
 public class CollectObjective : Objective
 {
+    [SerializeField] private ItemType _itemType;
     [SerializeField] private InventoryItemData _itemToCollect;
+
 
     public void CollectItem()
     {
-        for (int i = 0; i < InventorySystem.Instance.Inventory[ItemType.Item].Count; i++)
+        for (int i = 0; i < InventorySystem.Instance.Inventory[_itemType].Count; i++)
         {
-            if (InventorySystem.Instance.Inventory[ItemType.Item][i].Data.Id == _itemToCollect.Id)
+            if (InventorySystem.Instance.Inventory[_itemType][i].Data.Id == _itemToCollect.Id)
             {
                 Complete();
             }
