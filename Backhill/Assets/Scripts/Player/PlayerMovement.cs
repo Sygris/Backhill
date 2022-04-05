fde74b191b51d1855f7b1a4b91756f215186ccd8
Tiago Antunes <tiago.a.vista@gmail.com>
2022-04-05 10:38:57 +0100

    Fixed crouch for good

--- Backhill/Assets/Scripts/Player/PlayerMovement.cs
+++ Backhill/Assets/Scripts/Player/PlayerMovement.cs
@@ -15,7 +15,8 @@ public class PlayerMovement : MonoBehaviour
     [SerializeField] private float _crouchSpeed = 0.3f;
     [SerializeField] private float _standHeight = 2.0f;
     [SerializeField] private float _crouchHeight = 1.0f;
-    [SerializeField] private float _offset = 1.25f;
+    [SerializeField] private float _offsetStanding = 1.25f;
+    [SerializeField] private float _offsetCrouching = 0.25f;
     private bool _isCrouching = false;
 
     public bool IsCrouching { get { return _isCrouching; } }
@@ -79,7 +80,10 @@ public class PlayerMovement : MonoBehaviour
 
             // Moves the camera y position to be the same as the Character controller's y position
             var camPos = Camera.main.transform.position;
-            camPos.y = transform.position.y + _offset;
+
+            float offset = _isCrouching ? _offsetCrouching : _offsetStanding;
+
+            camPos.y = transform.position.y + offset;
 
             Camera.main.transform.position = camPos;
         }
