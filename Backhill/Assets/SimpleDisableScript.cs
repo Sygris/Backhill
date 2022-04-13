9f1013689ec0016103682c77f9557fb82de4fee0
LukyLewis2 <lewis.cozens@hotmail.co.uk>
2022-04-13 17:30:21 +0100

    + Notification animation fixed

---
+++ Backhill/Assets/SimpleDisableScript.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SimpleDisableScript : MonoBehaviour
+{
+    private void DisableObject()
+    {
+        gameObject.SetActive(false);
+    }
+}
