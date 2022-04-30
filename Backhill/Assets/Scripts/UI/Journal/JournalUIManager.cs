631d1abc98d66e70100cf6db4dea17541ceed916
Tiago Antunes <tiago.a.vista@gmail.com>
2022-04-30 11:07:51 +0100

    Notes name are now being displayed

--- Backhill/Assets/Scripts/UI/Journal/JournalUIManager.cs
+++ Backhill/Assets/Scripts/UI/Journal/JournalUIManager.cs
@@ -1,18 +1,47 @@
+using System.Collections.Generic;
 using TMPro;
 using UnityEngine;
 using UnityEngine.EventSystems;
 using UnityEngine.UI;
 
-public class JournalUIManager : MonoBehaviour, IPointerEnterHandler, IPointerExitHandler, IPointerClickHandler, IPointerDownHandler, IPointerUpHandler
+public class JournalUIManager : MonoBehaviour, /*IPointerEnterHandler,*/ /*IPointerExitHandler,*/ IPointerClickHandler, IPointerDownHandler, IPointerUpHandler
 {
     [Header("UI References")]
     [SerializeField] private TextMeshProUGUI _description;
     [SerializeField] private Image _backgroundImage;
+    [SerializeField] private GameObject _notesTitlesGrid;
+    private List<TextMeshProUGUI> _titles = new List<TextMeshProUGUI>();
 
     [Header("Note Color Settings")]
     [SerializeField] private Color _tabIdle;
     [SerializeField] private Color _tabHover;
     [SerializeField] private Color _tabSelected;
+    private int _noteSelected;
+
+    private void Start()
+    {
+        foreach (Transform title in _notesTitlesGrid.transform)
+        {
+            _titles.Add(title.GetComponent<TextMeshProUGUI>());
+        }
+
+        UpdateJournal();
+    }
+
+    private void OnEnable()
+    {
+        UpdateJournal();
+    }
+
+    private void UpdateJournal()
+    {
+        for (int i = 0; i < _titles.Count; i++)
+        {
+            if (InventorySystem.Instance.Inventory[ItemType.Note].Count <= 0 || i > (InventorySystem.Instance.Inventory[ItemType.Note].Count - 1)) return;
+
+            _titles[i].text = InventorySystem.Instance.Inventory[ItemType.Note][i].Data.DisplayName;
+        }
+    }
 
     public void OnPointerClick(PointerEventData eventData)
     {
@@ -22,6 +51,11 @@ public class JournalUIManager : MonoBehaviour, IPointerEnterHandler, IPointerExi
         _description.text = InventorySystem.Instance.Inventory[ItemType.Note][index].Data.Description;
         _backgroundImage.sprite = InventorySystem.Instance.Inventory[ItemType.Note][index].Data.Icon;
 
+        _titles[_noteSelected].color = Color.white;
+
+        _titles[index].color = _tabSelected;
+        _noteSelected = index;
+
     }
 
     public void OnPointerDown(PointerEventData eventData)
@@ -29,19 +63,18 @@ public class JournalUIManager : MonoBehaviour, IPointerEnterHandler, IPointerExi
         return;
     }
 
-    public void OnPointerEnter(PointerEventData eventData)
-    {
-        int index = eventData.pointerCurrentRaycast.gameObject.transform.GetSiblingIndex();
+    //public void OnPointerEnter(PointerEventData eventData)
+    //{
+    //    int index = eventData.pointerCurrentRaycast.gameObject.transform.GetSiblingIndex();
 
-        if (InventorySystem.Instance.Inventory[ItemType.Note].Count <= 0 || index > (InventorySystem.Instance.Inventory[ItemType.Note].Count - 1)) return;
+    //    if (InventorySystem.Instance.Inventory[ItemType.Note].Count <= 0 || index > (InventorySystem.Instance.Inventory[ItemType.Note].Count - 1)) return;
+    //}
 
-    }
-
-    public void OnPointerExit(PointerEventData eventData)
-    {
-        _description.text = string.Empty;
-        _backgroundImage.sprite = null;
-    }
+    //public void OnPointerExit(PointerEventData eventData)
+    //{
+    //    _description.text = string.Empty;
+    //    _backgroundImage.sprite = null;
+    //}
 
     public void OnPointerUp(PointerEventData eventData)
     {
