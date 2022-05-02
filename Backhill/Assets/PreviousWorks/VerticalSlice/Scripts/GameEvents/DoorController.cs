4c60ebef0b2b431c63063ab68941131ac1c491d5
LukyLewis2 <lewis.cozens@hotmail.co.uk>
2022-05-02 11:26:34 +0100

    + Door Error Fixed

--- Backhill/Assets/PreviousWorks/VerticalSlice/Scripts/GameEvents/DoorController.cs
+++ Backhill/Assets/PreviousWorks/VerticalSlice/Scripts/GameEvents/DoorController.cs
@@ -1,28 +1,28 @@
-using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+//using System.Collections;
+//using System.Collections.Generic;
+//using UnityEngine;
 
-public class DoorController : MonoBehaviour
-{
-    [SerializeField] private int _doorID;
-    private Animator _myAnim;
-    private void Start()
-    {
-        _myAnim = GetComponent<Animator>();
+//public class DoorController : MonoBehaviour
+//{
+//    [SerializeField] private int _doorID;
+//    private Animator _myAnim;
+//    private void Start()
+//    {
+//        _myAnim = GetComponent<Animator>();
 
-        GameEvents.Instance.TriggerDoorOpen += OpenDoor;
-        GameEvents.Instance.TriggerDoorClose += CloseDoor;
-    }
+//        GameEvents.Instance.TriggerDoorOpen += OpenDoor;
+//        GameEvents.Instance.TriggerDoorClose += CloseDoor;
+//    }
 
-    private void OpenDoor(int id)
-    {
-        if (id == _doorID)
-            _myAnim.SetTrigger("OpenDoor");
-    }
+//    private void OpenDoor(int id)
+//    {
+//        if (id == _doorID)
+//            _myAnim.SetTrigger("OpenDoor");
+//    }
 
-    private void CloseDoor(int id)
-    {
-        if (id == _doorID)
-            _myAnim.SetTrigger("CloseDoor");
-    }
-}
+//    private void CloseDoor(int id)
+//    {
+//        if (id == _doorID)
+//            _myAnim.SetTrigger("CloseDoor");
+//    }
+//}
